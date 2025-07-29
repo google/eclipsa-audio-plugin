334c4fa0d4bb246ecde3e226d2eee6c63ac26004
harmankacx <harman.khungura@a-cx.com>
2025-07-29 11:06:20 -0700

    reading update data

--- common/components/src/AEStripComponent.cpp
+++ common/components/src/AEStripComponent.cpp
@@ -226,6 +226,7 @@ juce::Rectangle<int> AEStripComponent::setLightsSpacing(
 
 void AEStripComponent::timerCallback() {
   size_t i = 0;
+  channelMonitorData_.channelLoudnesses.read(channelLoudnessesRead_);
   for (auto channelIndex : channelsSet_) {
     channelIndicators[i]->setColour(determineColourIndex(channelIndex));
     channelIndicators[i]->repaint();
