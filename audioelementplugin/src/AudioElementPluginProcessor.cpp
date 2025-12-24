87c4194fa8d6487c263b057d9cca957fc3911612
Erik Jourgensen <erikjourgensen@gmail.com>
2025-12-24 13:05:33 -0800

    Updated AudioElementProcessor for dll loading

--- audioelementplugin/src/AudioElementPluginProcessor.cpp
+++ audioelementplugin/src/AudioElementPluginProcessor.cpp
@@ -51,6 +51,27 @@ AudioElementPluginProcessor::AudioElementPluginProcessor()
       syncClient_(&audioElementSpatialLayoutRepository_, 2134),
       automationParametersTreeState(*this),
       trackName_("") {
+
+#ifdef WIN32
+  // Go to the plugin directory
+  const auto pluginFile = juce::File::getSpecialLocation(
+      juce::File::SpecialLocationType::currentExecutableFile);
+  const auto pluginDirectory = pluginFile.getParentDirectory();
+
+  // Find the DLLs in the plugin directory
+  auto dllFiles = pluginDirectory.findChildFiles(
+      juce::File::findFiles,
+      false,
+      "*.dll"
+  );
+
+  // Load each DLL
+  for (const auto& dllFile : dllFiles) {
+    std::string dllPath = dllFile.getFullPathName().toStdString();
+    HMODULE result = LoadLibraryA(dllPath.c_str());
+  }
+#endif
+
   elevationListener_.setListeners(&automationParametersTreeState,
                                   &audioElementSpatialLayoutRepository_);
 
