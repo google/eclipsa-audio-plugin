21ac65e59561d03dd09eae5df8e206c935cf9fb4
Erik Jourgensen <erik.jourgensen@a-cx.com>
2026-01-07 15:01:50 -0800

    Fixed Renderer Processor clang formatting

--- rendererplugin/src/RendererProcessor.cpp
+++ rendererplugin/src/RendererProcessor.cpp
@@ -271,7 +271,7 @@ juce::AudioProcessorEditor* RendererProcessor::createEditor() {
 void RendererProcessor::getStateInformation(juce::MemoryBlock& destData) {
   LOG_ANALYTICS(instanceId_, "RendererProcessor getStateInformation \n");
 
-// Always add the latest version attribute to the XML state
+  // Always add the latest version attribute to the XML state
 #if defined(ECLIPSA_VERSION)
   LOG_ANALYTICS(instanceId_, "Renderer Plugin setting config version to \n" +
                                  std::string(ECLIPSA_VERSION));
@@ -422,7 +422,7 @@ juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter() {
 }
 
 void RendererProcessor::checkManualOfflineStartStop() {
-// This is utilized by debug builds to perform the manual bounce operation
+  // This is utilized by debug builds to perform the manual bounce operation
 #if JUCE_DEBUG
   juce::SpinLock::ScopedLockType realtimeLock(realtimeLock_);
   FileExport configParams = fileExportRepository_.get();
@@ -453,10 +453,12 @@ void RendererProcessor::valueTreePropertyChanged(
     LOG_ANALYTICS(instanceId_, mainBusInfo);
   }
 }
+
 void RendererProcessor::valueTreeChildAdded(
     juce::ValueTree& parentTree, juce::ValueTree& childWhichHasBeenAdded) {
   checkManualOfflineStartStop();
 }
+
 void RendererProcessor::valueTreeChildRemoved(
     juce::ValueTree& parentTree, juce::ValueTree& childWhichHasBeenRemoved,
     int indexFromWhichChildWasRemoved) {
