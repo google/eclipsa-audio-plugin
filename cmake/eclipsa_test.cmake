773b994ad33e4c92d3c0531753ac5815e2f079b1
Erik Jourgensen <erik.jourgensen@a-cx.com>
2026-02-12 12:48:38 -0800

    updated eclipsa_test.cmake to fix windows crash on tests in workflows

--- cmake/eclipsa_test.cmake
+++ cmake/eclipsa_test.cmake
@@ -17,53 +17,35 @@
 # test_source:  Test source file name.
 # test_libs:    Test link libraries as ';' separated list.
 function(eclipsa_add_test test_name test_source test_libs)
-    # Construct the absolute path to the test source file
     set(absolute_test_source "${CMAKE_CURRENT_SOURCE_DIR}/${test_source}")
 
-    # Append the absolute test source path to a global list of sources
     get_property(test_sources GLOBAL PROPERTY ECLIPSA_TEST_SOURCES)
     set_property(GLOBAL PROPERTY ECLIPSA_TEST_SOURCES "${test_sources};${absolute_test_source}")
 
-    # Add codec libraries for all test targets
-    if (APPLE)
-        set(LIBIAMF_CODEC_PATH "${CMAKE_SOURCE_DIR}/third_party/libiamf/lib/macos")
-        list(FIND test_libs "opus" opus_found)
-        if (opus_found EQUAL -1)
-            list(APPEND test_libs "${LIBIAMF_CODEC_PATH}/libopus.a")
-        endif ()
-        list(FIND test_libs "ogg" ogg_found)
-        if (ogg_found EQUAL -1)
-            list(APPEND test_libs "${LIBIAMF_CODEC_PATH}/libogg.a")
-        endif ()
-    elseif (WIN32)
-        set(LIBIAMF_CODEC_PATH "${CMAKE_SOURCE_DIR}/third_party/libiamf/lib/Windows")
-        list(FIND test_libs "opus" opus_found)
-        if (opus_found EQUAL -1)
-            list(APPEND test_libs "$<IF:$<CONFIG:Debug>,${LIBIAMF_CODEC_PATH}/Debug/opus.lib,${LIBIAMF_CODEC_PATH}/Release/opus.lib>")
-        endif ()
-        list(FIND test_libs "ogg" ogg_found)
-        if (ogg_found EQUAL -1)
-            list(APPEND test_libs "$<IF:$<CONFIG:Debug>,${LIBIAMF_CODEC_PATH}/Debug/ogg.lib,${LIBIAMF_CODEC_PATH}/Release/ogg.lib>")
-        endif ()
+    list(FIND test_libs "opus" opus_found)
+    if (opus_found EQUAL -1)
+        list(APPEND test_libs opus)
     endif ()
 
-    # Add IAMF include directories if 'iamf' or 'iamfdec_utils' is requested
+    list(FIND test_libs "ogg" ogg_found)
+    if (ogg_found EQUAL -1)
+        list(APPEND test_libs ogg)
+    endif ()
+
+    # Dependency logic for iamf/iamfdec_utils
     if ("${test_libs}" MATCHES "iamf" OR "${test_libs}" MATCHES "iamfdec_utils")
         if (NOT DEFINED LIBIAMF_INCLUDE_DIRS)
             message(WARNING "LIBIAMF_INCLUDE_DIRS not defined, but required by ${test_name}")
         endif ()
     endif ()
 
-    # If iamfdec_utils was requested, make sure iamf is also linked (dependency)
     if (TARGET iamf AND "${test_libs}" MATCHES "iamfdec_utils")
         list(FIND test_libs "iamf" iamf_already_linked)
         if (iamf_already_linked EQUAL -1)
             list(APPEND test_libs iamf)
-            message(STATUS "Also linking core 'iamf' library as dependency for ${test_name}")
         endif ()
     endif ()
 
-    # Append the test libraries to global list AFTER all modifications
     get_property(test_link_libs GLOBAL PROPERTY ECLIPSA_TEST_LINK_LIBS)
     set_property(GLOBAL PROPERTY ECLIPSA_TEST_LINK_LIBS "${test_link_libs};${test_libs}")
 endfunction()
