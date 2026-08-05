a96a8613c6487186f3b4a83f82f0e45568f05e52
Branden Crawford <branden.crawford@a-cx.com>
2026-08-05 13:01:28 -0700

    Format fix

--- common/processors/file_output/FileOutputProcessor.cpp
+++ common/processors/file_output/FileOutputProcessor.cpp
@@ -308,7 +308,7 @@ void FileOutputProcessor::checkAudioVideoDurationMismatch(
   }
   // Failure tolerance is reasonably large to avoid false positives as 0.5s of
   // silence is unlikely to be noticed and video/audio recordings seem to often
-  // have a mismatch of ~2-10 frames, which is ~0.05-0.2s at 48kHz. 
+  // have a mismatch of ~2-10 frames, which is ~0.05-0.2s at 48kHz.
   constexpr double kDurationMismatchToleranceSec = 0.5;
   const double kAudioDurationSec =
       static_cast<double>(framesWritten_) / sampleRate_;
